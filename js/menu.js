bef6afb0f78955a03fe9c98e6565be4953c526dc
Willian Amorim <willian.c.amorim@outlook.com>
2025-07-11 22:19:56 -0300

    atualização 70%

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,21 @@
 const btnHamburguer = document.querySelector('.hamburguer');
 const menu = document.getElementById('menu');
+const btnTopo = document.getElementById('btnTopo');
 
 btnHamburguer.addEventListener('click', () => {
   const expanded = btnHamburguer.getAttribute('aria-expanded') === 'true';
   btnHamburguer.setAttribute('aria-expanded', !expanded);
   menu.classList.toggle('active');
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    btnTopo.classList.add('show');
+  } else {
+    btnTopo.classList.remove('show');
+  }
+});
+
+btnTopo.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
